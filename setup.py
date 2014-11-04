83859dda9aec5ca47a4bd7b7eba092cdfe89d7ed
Joshua Gilman <joshuagilman@gmail.com>
2014-11-03 19:16:41 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name="neolib2",
+    packages=["requests", "lxml"],
+    version="0.1.1",
+    description="Neopets automation library for Python",
+    author="Joshua Gilman",
+    author_email="joshuagilman@gmail.com",
+    url="https://github.com/jmgilman/neolib2",
+    download_url="https://github.com/jmgilman/neolib2/archive/master.zip",
+    keywords=["Neopets", "automation", "library"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description=""
+    )
