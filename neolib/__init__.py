902b7ab0de88f7767a305c8d43721ebb8d607990
Joshua Gilman <joshuagilman@gmail.com>
2014-11-19 20:46:44 -0800

    Moved example programs to another repository

--- neolib/__init__.py
+++ neolib/__init__.py
@@ -1,9 +1,9 @@
 import logging
 import logging.config
 
-# A filter which prevents high level log records from being passed to the
-# low level console handler (thus causing duplicate messages)
+
 class LevelFilter(logging.Filter):
+    """ Filters logs so high levels don't show up on low level consoles """
     def __init__(self, name=''):
         self.name = name
 
@@ -18,25 +18,40 @@ class LevelFilter(logging.Filter):
 # the internal logging. For further information on how this dictionary should
 # be configured please refer to the python help documents.
 CONFIG = {'version': 1,
-          'formatters': { 'debug': {'format': '%(message)s'},
-                          'error': { 'format': '[%(asctime)s] %(name)s (%(levelname)s) Line %(lineno)d in %(filename)s : %(message)s' }
-
-                        },
-          'filters': { 'level_filter': {'name': 'neolib',
-                                        '()': LevelFilter}
-                     },
-          'handlers': { 'console_low': {'class': 'logging.StreamHandler',
-                                        'level': 'DEBUG',
-                                        'formatter': 'debug',
-                                        'filters': ['level_filter']},
-                        'console_high': {'class': 'logging.StreamHandler',
-                                         'level': 'ERROR',
-                                         'formatter': 'error'}
-                      },
-          'loggers': { 'neolib': {'level': 'DEBUG',
-                                  'handlers': ['console_low', 'console_high']}
-                     }
-         }
+          'formatters': {
+              'debug': {
+                  'format': '%(message)s'
+                  },
+              'error': {
+                  'format': '[%(asctime)s] %(name)s (%(levelname)s) Line %(lineno)d in %(filename)s : %(message)s'
+                  }
+              },
+          'filters': {
+              'level_filter': {
+                  'name': 'neolib',
+                  '()': LevelFilter
+                  }
+              },
+          'handlers': {
+              'console_low': {
+                  'class': 'logging.StreamHandler',
+                  'level': 'DEBUG',
+                  'formatter': 'debug',
+                  'filters': ['level_filter']
+                  },
+              'console_high': {
+                  'class': 'logging.StreamHandler',
+                  'level': 'ERROR',
+                  'formatter': 'error'
+                  }
+              },
+          'loggers': {
+              'neolib': {
+                  'level': 'DEBUG',
+                  'handlers': ['console_low', 'console_high']
+                  }
+              }
+          }
 
 # Set the configuration for the base logger 'neolib'
 logging.config.dictConfig(CONFIG)
